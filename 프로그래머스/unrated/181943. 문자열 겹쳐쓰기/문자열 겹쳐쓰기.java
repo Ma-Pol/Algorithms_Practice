13d081d7557fbdeefb075d36e08a6a9f01eb1d03
SEUNG-HYEON <121948211+Ma-Pol@users.noreply.github.com>
2023-09-10 22:04:04 +0900

    Update 문자열 겹쳐쓰기.java

--- 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
+++ 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -1,15 +1,14 @@
 class Solution {
     public String solution(String my_string, String overwrite_string, int s) {
-        String result = "";
+        SString first = "", last = "";
         int osLength = overwrite_string.length(), msLength = my_string.length();
         
         if(1 <= osLength && osLength <= msLength && msLength <= 1000 && 
            0 <= s && s <= msLength - osLength) {
-            result += my_string.substring(0, s);
-            result += overwrite_string;
-            result += my_string.substring(s + osLength, msLength);
+            first += my_string.substring(0, s);
+            last += my_string.substring(s + osLength, msLength);
         }
         
-        return result;
+        return first + overwrite_string + last;
     }
 }
