17049b4c08ff037c202d11c04162507ef80e9d3f
SEUNG-HYEON <121948211+Ma-Pol@users.noreply.github.com>
2023-09-18 00:59:07 +0900

    [unrated] Title: 글자 지우기, Time: 0.14 ms, Memory: 70.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181900. 글자 지우기/글자 지우기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(String myString, int[] indices) {
+        StringBuilder sb = new StringBuilder(myString);
+        
+        for(int i : indices)
+            sb.setCharAt(i, ' ');
+        
+        return sb.toString().replace(" ", "");
+    }
+}
