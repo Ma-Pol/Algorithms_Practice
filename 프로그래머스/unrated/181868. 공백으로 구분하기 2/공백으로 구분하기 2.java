318605e7e199b9a1109d4009ddce9ca4743ab0ce
Yi, Seung Hyeon <121948211+Ma-Pol@users.noreply.github.com>
2023-09-19 23:20:42 +0900

    [unrated] Title: 공백으로 구분하기 2, Time: 0.07 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181868. 공백으로 구분하기 2/공백으로 구분하기 2.java
+++ 프로그래머스/unrated/181868. 공백으로 구분하기 2/공백으로 구분하기 2.java
@@ -2,9 +2,12 @@ import java.util.*;
 
 class Solution {
     public String[] solution(String myString) {
-        return Arrays
-                .stream(myString.trim().split(" "))
-                .filter(str -> !str.equals(""))
-                .toArray(String[]::new);
+        
+        // return Arrays
+        //         .stream(myString.trim().split(" "))
+        //         .filter(str -> !str.equals(""))
+        //         .toArray(String[]::new);
+        
+        return myString.trim().split("[ ]+");
     }
 }
