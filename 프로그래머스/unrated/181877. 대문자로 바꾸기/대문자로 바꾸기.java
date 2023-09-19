3d949c17f221527850682e48a2cc719e6e856969
SEUNG-HYEON <121948211+Ma-Pol@users.noreply.github.com>
2023-09-19 16:40:50 +0900

    [unrated] Title: 대문자로 바꾸기, Time: 0.03 ms, Memory: 80.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181877. 대문자로 바꾸기/대문자로 바꾸기.java
+++ 프로그래머스/unrated/181877. 대문자로 바꾸기/대문자로 바꾸기.java
@@ -1,14 +1,5 @@
 class Solution {
     public String solution(String myString) {
-        String result = "";
-        
-        // 아스키코드 65 ~ 90 : 대문자
-        // 아스키코드 97 ~ 122 : 소문자
-        for(char ch : myString.toCharArray()) {
-            if(97 <= ch && ch <= 122) ch -= 32;
-            result += ch;
-        }
-        
-        return result;
+        return myString.toUpperCase();
     }
 }
