88a3a8d07a427a13aa15b304217a49664574c62b
Yi, Seung Hyeon <121948211+Ma-Pol@users.noreply.github.com>
2023-09-20 16:20:17 +0900

    [unrated] Title: 특별한 이차원 배열 2, Time: 0.02 ms, Memory: 74.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int[][] arr) {
+        int result = 1;
+        
+        loop:
+        for(int i = 0; i < arr.length; i++)
+            for(int j = 0; j < arr[i].length; j++)
+                if(arr[i][j] != arr[j][i]) {
+                    result = 0;
+                    break loop;
+                }
+        
+        return result;
+    }
+}
