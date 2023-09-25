898d6ed3bdc5d17e3a8d13417779c38be44c34b3
Yi, Seung Hyeon <121948211+Ma-Pol@users.noreply.github.com>
2023-09-25 21:46:48 +0900

    [level 0] Title: 짝수의 합, Time: 0.01 ms, Memory: 66 MB -BaekjoonHub

--- 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.java
+++ 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.java
@@ -2,8 +2,8 @@ class Solution {
     public int solution(int n) {
         int result = 0;
         
-        for(int i = 1; i <= n; i++)
-            if(i % 2 == 0) result += i;
+        for(int i = 2; i <= n; i += 2)
+            result += i;
         
         return result;
     }
