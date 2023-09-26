9a778f0004f80aefca5d3246ed1eb3fc90449589
Yi, Seung Hyeon <121948211+Ma-Pol@users.noreply.github.com>
2023-09-26 21:41:17 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.03 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.java
+++ 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.java
@@ -1,8 +1,6 @@
 class Solution {
     public int solution(int n) {
-        double sqrt = Math.sqrt(n);
-        
         // 제곱근이 정수면 자연수의 제곱수
-        return sqrt % 1.0 == 0.0 ? 1 : 2;
+        return Math.sqrt(n) % 1 == 0 ? 1 : 2;
     }
 }
