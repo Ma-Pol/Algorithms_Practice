f0239f2ce2ce3c25d86565466ad7142750364fd9
Yi, Seung Hyeon <121948211+Ma-Pol@users.noreply.github.com>
2023-09-28 02:07:49 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.11 ms, Memory: 72.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120853. 컨트롤 제트/컨트롤 제트.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(String s) {
+        String[] sArr = s.split(" ");
+        int result = 0;
+        
+        for(int i = 0; i < sArr.length; i++)
+            if(sArr[i].equals("Z"))
+                result -= Integer.parseInt(sArr[i - 1]);
+            else 
+                result += Integer.parseInt(sArr[i]);
+        
+        return result;
+    }
+}
