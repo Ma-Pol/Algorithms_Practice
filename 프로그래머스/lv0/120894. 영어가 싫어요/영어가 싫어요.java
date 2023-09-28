efcfc4c9258d1cb577bad240612ae1362f66256e
Yi, Seung Hyeon <121948211+Ma-Pol@users.noreply.github.com>
2023-09-29 01:02:03 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.40 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.java
+++ 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.java
@@ -2,9 +2,8 @@ class Solution {
     public long solution(String numbers) {
         String[] numStrArr = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
         
-        for(int i = 0; i < numStrArr.length; i++) {
+        for(int i = 0; i < numStrArr.length; i++)
             numbers = numbers.replaceAll(numStrArr[i], Integer.toString(i));
-        }
         
         return Long.parseLong(numbers);
     }
