8e313ce955284b126f3e2674533ccb400c507c73
Yi, Seung Hyeon <121948211+Ma-Pol@users.noreply.github.com>
2023-09-30 01:07:04 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.10 ms, Memory: 67.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120902. 문자열 계산하기/문자열 계산하기.java
+++ 프로그래머스/lv0/120902. 문자열 계산하기/문자열 계산하기.java
@@ -1,30 +1,14 @@
-import java.util.*;
-
 class Solution {
     public int solution(String myString) {
         String[] strArr = myString.split(" ");
-        String op = "";
-        Stack<Integer> stack = new Stack<>();
+        int result = 0, op = 1;
         
-        for(String str : strArr) {
-            switch(str) {
-                case "+":
-                case "-":
-                    op = str;
-                    break;
-                    
-                default:
-                    stack.push(Integer.parseInt(str));
-                    break;
-            }
-            
-            if(stack.size() == 2) {
-                int b = stack.pop();
-                int a = stack.pop();
-                stack.push(op.equals("+") ? a + b : a - b);
-            }
-        }
+        for(String str : strArr)
+            if(str.equals("+") || str.equals("-"))
+                op = str.equals("+") ? 1 : -1;
+            else
+                result += Integer.parseInt(str) * op;
         
-        return stack.pop();
+        return result;
     }
 }
