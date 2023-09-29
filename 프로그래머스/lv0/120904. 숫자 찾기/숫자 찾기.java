35224143449f5186371f517116c12b1fae4d40af
Yi, Seung Hyeon <121948211+Ma-Pol@users.noreply.github.com>
2023-09-30 01:14:39 +0900

    [level 0] Title: 숫자 찾기, Time: 0.15 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.java
+++ 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.java
@@ -8,5 +8,8 @@ class Solution {
                 result = i + 1;
         
         return result;
+        
+        // 좀 느리지만 짧게 해결하는 방법
+        // return (" " + num).indexOf(String.valueOf(k));
     }
 }
