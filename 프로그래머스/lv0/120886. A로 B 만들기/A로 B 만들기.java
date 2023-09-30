5078001f514ec245aa4e07c1f7f25dd8c9f53518
Yi, Seung Hyeon <121948211+Ma-Pol@users.noreply.github.com>
2023-10-01 01:20:56 +0900

    [level 0] Title: A로 B 만들기, Time: 0.50 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/lv0/120886. A로 B 만들기/A로 B 만들기.java
+++ 프로그래머스/lv0/120886. A로 B 만들기/A로 B 만들기.java
@@ -1,12 +1,13 @@
+import java.util.*;
+
 class Solution {
     public int solution(String before, String after) {
+        char[] a = before.toCharArray();
+        char[] b = after.toCharArray();
         
-        for(int i = 0; i < before.length(); i++) {
-            String temp = before.substring(i, i + 1);
-            
-            after = after.replaceFirst(temp, "");
-        }
-        
-        return after.length() == 0 ? 1 : 0;
+        Arrays.sort(a);
+        Arrays.sort(b);
+
+        return new String(a).equals(new String(b)) ? 1 :0;
     }
 }
