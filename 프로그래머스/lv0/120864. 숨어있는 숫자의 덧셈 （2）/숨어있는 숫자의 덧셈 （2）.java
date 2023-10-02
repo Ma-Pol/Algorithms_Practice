f6a078c7403bfe4158cfda91eea12ee441c371ca
Yi, Seung Hyeon <121948211+Ma-Pol@users.noreply.github.com>
2023-10-02 11:40:40 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.15 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.java
+++ 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.java
@@ -1,7 +1,7 @@
 class Solution {
     public int solution(String myString) {
         int result = 0;
-        String[] strArr = myString.replaceAll("[a-zA-Z]", " ").split(" ");
+        String[] strArr = myString.split("[a-zA-Z]");
         
         for(String str : strArr)
             if(!str.equals("")) result += Integer.parseInt(str);
