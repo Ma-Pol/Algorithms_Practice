8821b6a274ee743ab1e33841431acf99541787c0
Yi, Seung Hyeon <121948211+Ma-Pol@users.noreply.github.com>
2023-10-04 20:53:34 +0900

    [level 0] Title: 평행, Time: 0.02 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/lv0/120875. 평행/평행.java
+++ 프로그래머스/lv0/120875. 평행/평행.java
@@ -1,33 +1,35 @@
 class Solution {
+    
+    private int[][] dots;
+    
     public int solution(int[][] dots) {
         int result = 0;
         
-        int x1 = dots[0][0];
-        int y1 = dots[0][1];
+        this.dots = dots;
+
+        if(slope(0, 1, 2, 3)) result++;
+        else if(slope(0, 2, 1, 3)) result++;
+        else if(slope(0, 3, 1, 2)) result++;
         
-        int x2 = dots[1][0];
-        int y2 = dots[1][1];
+        return result;
+    }
+    
+    private boolean slope(int a, int b, int c, int d) {
+        int x1 = this.dots[a][0];
+        int y1 = this.dots[a][1];
         
-        int x3 = dots[2][0];
-        int y3 = dots[2][1];
+        int x2 = this.dots[b][0];
+        int y2 = this.dots[b][1];
         
-        int x4 = dots[3][0];
-        int y4 = dots[3][1];
+        int x3 = this.dots[c][0];
+        int y3 = this.dots[c][1];
         
-        // 경우의 수 : 3개
+        int x4 = this.dots[d][0];
+        int y4 = this.dots[d][1];
+        
         double slope1 = (double) (y2 - y1) / (x2 - x1);
         double slope2 = (double) (y4 - y3) / (x4 - x3);
         
-        double slope3 = (double) (y3 - y1) / (x3 - x1);
-        double slope4 = (double) (y4 - y2) / (x4 - x2);
-        
-        double slope5 = (double) (y4 - y1) / (x4 - x1);
-        double slope6 = (double) (y3 - y2) / (x3 - x2);
-
-        if(slope1 == slope2) result++;
-        else if(slope3 == slope4) result++;
-        else if(slope5 == slope6) result++;
-        
-        return result;
+        return slope1 == slope2;
     }
 }
