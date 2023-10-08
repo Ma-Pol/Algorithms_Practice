4f4478f61d70915b82899d6785363d4b4d159a5f
Yi, Seung Hyeon <121948211+Ma-Pol@users.noreply.github.com>
2023-10-09 00:04:56 +0900

    [level 0] Title: 등수 매기기, Time: 0.02 ms, Memory: 77.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120882. 등수 매기기/등수 매기기.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] solution(int[][] score) {
+        int[] result = new int[score.length];
+        double[] avg = new double[score.length];
+        
+        // 각 학생의 평균 점수를 저장(double형)
+        for(int i = 0; i < score.length; i++)
+            avg[i] = (double) (score[i][0] + score[i][1]) / 2.0;
+        
+        // 각 학생의 평균 점수를 비교, 등수를 저장
+        for(int i = 0; i < avg.length; i++) {
+            int rank = 1;
+            
+            for(int j = 0; j < avg.length; j++)
+                if(avg[i] < avg[j])
+                    rank++;
+            
+            result[i] = rank;
+        }
+        
+        return result;
+    }
+}
