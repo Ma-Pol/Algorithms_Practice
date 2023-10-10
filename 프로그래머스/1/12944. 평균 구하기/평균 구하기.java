8af096564b4ae3aceb285a754345815a6ce620c1
Yi, Seung Hyeon <121948211+Ma-Pol@users.noreply.github.com>
2023-10-10 14:05:04 +0900

    [level 1] Title: 평균 구하기, Time: 0.04 ms, Memory: 69.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public double solution(int[] arr) {
+        double result = 0;
+        
+        for(int num : arr)
+            result += num;
+        
+        return result / arr.length;
+    }
+}
