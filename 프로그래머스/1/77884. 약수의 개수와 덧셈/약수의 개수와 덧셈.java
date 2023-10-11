50c11322d0eb6a3d0687cfd282e78c480a3e623a
Yi, Seung Hyeon <121948211+Ma-Pol@users.noreply.github.com>
2023-10-11 15:34:45 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.04 ms, Memory: 80.1 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.java
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.java
@@ -2,25 +2,36 @@ class Solution {
     public int solution(int left, int right) {
         int result = 0;
         
+        // 제곱수인 경우, 약수의 개수는 홀수 (Math.sqrt(i) % 1.0 != 0)
+        // 제곱수가 아닌 경우, 약수의 개수는 짝수 (Math.sqrt(i) % 1.0 == 0)
         for(int i = left; i <= right; i++)
-            result += i * (isEven(i) ? 1 : -1);
+            result += i * (Math.sqrt(i) % 1.0 != 0 ? 1 : -1);
         
         return result;
     }
     
-    // 약수의 개수를 구해 짝수면 true, 홀수면 false를 리턴
-    private boolean isEven(int num) {
-        int sqrt = (int) Math.sqrt(num);
-        int count = 0;
+//     public int solution(int left, int right) {
+//         int result = 0;
         
-        for(int i = 1; i <= sqrt; i++)
-            if(num % i == 0) {
-                count++;
+//         for(int i = left; i <= right; i++)
+//             result += i * (isEven(i) ? 1 : -1);
+        
+//         return result;
+//     }
+    
+//     // 약수의 개수를 구해 짝수면 true, 홀수면 false를 리턴
+//     private boolean isEven(int num) {
+//         int sqrt = (int) Math.sqrt(num);
+//         int count = 0;
+        
+//         for(int i = 1; i <= sqrt; i++)
+//             if(num % i == 0) {
+//                 count++;
                 
-                if(num / i != 1 && num / i != sqrt)
-                    count++;
-            }
+//                 if(num / i != 1 && num / i != sqrt)
+//                     count++;
+//             }
         
-        return count % 2 == 0;
-    }
+//         return count % 2 == 0;
+//     }
 }
