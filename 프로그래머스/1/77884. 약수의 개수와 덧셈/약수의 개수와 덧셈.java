9189cd3a4ba80115c30cb4ec4e14c1a04e255454
Yi, Seung Hyeon <121948211+Ma-Pol@users.noreply.github.com>
2023-10-11 15:29:44 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.06 ms, Memory: 73.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int solution(int left, int right) {
+        int result = 0;
+        
+        for(int i = left; i <= right; i++)
+            result += i * (isEven(i) ? 1 : -1);
+        
+        return result;
+    }
+    
+    // 약수의 개수를 구해 짝수면 true, 홀수면 false를 리턴
+    private boolean isEven(int num) {
+        int sqrt = (int) Math.sqrt(num);
+        int count = 0;
+        
+        for(int i = 1; i <= sqrt; i++)
+            if(num % i == 0) {
+                count++;
+                
+                if(num / i != 1 && num / i != sqrt)
+                    count++;
+            }
+        
+        return count % 2 == 0;
+    }
+}
