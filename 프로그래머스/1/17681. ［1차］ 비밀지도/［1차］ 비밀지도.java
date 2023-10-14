f9c74f9168c1e218f48bb3c0d9018235b3c7d666
Yi, Seung Hyeon <121948211+Ma-Pol@users.noreply.github.com>
2023-10-15 01:09:44 +0900

    [level 1] Title: [1차] 비밀지도, Time: 1.87 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -2,13 +2,21 @@ class Solution {
     public String[] solution(int n, int[] arr1, int[] arr2) {
         String[] result = new String[n];
         
+        // 남의 풀이 바탕으로 다시 만든 풀이
         for(int i = 0; i < n; i++) {
-            long temp = Long.parseLong(Integer.toString(arr1[i], 2)) + Long.parseLong(Integer.toString(arr2[i], 2));
-            String tempStr = Long.toString(temp);
+            String temp = Integer.toBinaryString(arr1[i] | arr2[i]);
             
-            
-            result[i] = ("0".repeat(n - tempStr.length()) + tempStr).replaceAll("[^0]", "#").replaceAll("0", " ");
+            result[i] = ("0".repeat(n - temp.length()) + temp).replaceAll("[^0]", "#").replaceAll("0", " ");
         }
+        
+//         // 내 풀이
+//         for(int i = 0; i < n; i++) {
+//             long temp = Long.parseLong(Integer.toString(arr1[i], 2)) + Long.parseLong(Integer.toString(arr2[i], 2));
+//             String tempStr = Long.toString(temp);
+            
+            
+//             result[i] = ("0".repeat(n - tempStr.length()) + tempStr).replaceAll("[^0]", "#").replaceAll("0", " ");
+//         }
         
         return result;
     }
