75e13923f9187e67e6dbd94d1866d09012411ab0
Yi, Seung Hyeon <121948211+Ma-Pol@users.noreply.github.com>
2023-10-14 16:27:26 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.38 ms, Memory: 72.5 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -1,26 +1,39 @@
-import java.util.*;
-
+// 조금 느리지만 짧게
 class Solution {
     public int solution(String s) {
-        StringBuilder num = new StringBuilder();
-        StringBuilder numStr = new StringBuilder();
-        List<String> numList = new ArrayList<>(
-            Arrays.asList("zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
-        );
+        String[] numArr = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
         
-        for(char temp : s.toCharArray())
-            if('0' <= temp && temp <= '9')
-                num.append(temp);
-            else {
-                numStr.append(temp);
-                int index = numList.indexOf(numStr.toString());
-                
-                if(index != -1) {
-                    num.append(index);
-                    numStr.delete(0, numStr.length());
-                }
-            }
-        
-        return Integer.parseInt(num.toString());
+        for(int i = 0; i < numArr.length; i++)
+            s = s.replaceAll(numArr[i], Integer.toString(i));
+            
+        return Integer.parseInt(s);
     }
 }
+
+// // 조금 길지만 빠르게
+// import java.util.*;
+
+// class Solution {
+//     public int solution(String s) {
+//         StringBuilder num = new StringBuilder();
+//         StringBuilder numStr = new StringBuilder();
+//         List<String> numList = new ArrayList<>(
+//             Arrays.asList("zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
+//         );
+        
+//         for(char temp : s.toCharArray())
+//             if('0' <= temp && temp <= '9')
+//                 num.append(temp);
+//             else {
+//                 numStr.append(temp);
+//                 int index = numList.indexOf(numStr.toString());
+                
+//                 if(index != -1) {
+//                     num.append(index);
+//                     numStr.delete(0, numStr.length());
+//                 }
+//             }
+        
+//         return Integer.parseInt(num.toString());
+//     }
+// }
