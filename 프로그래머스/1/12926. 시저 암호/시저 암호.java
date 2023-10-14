1b9a1dea5f3659bbad2a4483290e2b11db360a51
Yi, Seung Hyeon <121948211+Ma-Pol@users.noreply.github.com>
2023-10-14 15:57:41 +0900

    [level 1] Title: 시저 암호, Time: 0.89 ms, Memory: 72 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.java
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.java
@@ -2,20 +2,17 @@ class Solution {
     public String solution(String s, int n) {
         StringBuilder sb = new StringBuilder();
         
-        for(char ch : s.toCharArray()) {
+        for(char ch : s.toCharArray())
             if(ch == ' ') {
                 sb.append(" ");
                 continue;
             }
             
-            if('A' <= ch && ch <= 'Z') {
+            else if('A' <= ch && ch <= 'Z')
                 sb.append((char) ((ch - 65 + n) % 26 + 65));
-            } 
             
-            else if('a' <= ch && ch <= 'z') {
+            else if('a' <= ch && ch <= 'z')
                 sb.append((char) ((ch - 97 + n) % 26 + 97));
-            }
-        }
         
         return sb.toString();
     }
