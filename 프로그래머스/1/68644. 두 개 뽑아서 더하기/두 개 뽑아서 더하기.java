ab6d894e3a768a892806cb1fb5c9a205a15c00f3
Yi, Seung Hyeon <121948211+Ma-Pol@users.noreply.github.com>
2023-10-15 18:25:05 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 1.15 ms, Memory: 72.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] numbers) {
+        Set<Integer> set = new HashSet<>();
+        int[] result = null;
+        int idx = 0;
+        
+        for(int i = 0; i < numbers.length - 1; i++)
+            for(int j = i + 1; j < numbers.length; j++)
+                set.add(numbers[i] + numbers[j]);
+        
+        Iterator iter = set.iterator();
+        result = new int[set.size()];
+        
+        while(iter.hasNext())
+            result[idx++] = Integer.parseInt(iter.next().toString());
+        
+        Arrays.sort(result);
+        
+        return result;
+    }
+}
