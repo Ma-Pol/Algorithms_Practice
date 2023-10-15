bfdfbe1fa8410cc3f2ea58717ba32c3892b7b953
Yi, Seung Hyeon <121948211+Ma-Pol@users.noreply.github.com>
2023-10-16 01:03:22 +0900

    [level 1] Title: 콜라 문제, Time: 0.03 ms, Memory: 76.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
@@ -0,0 +1,16 @@
+class Solution {
+    // 콜라를 받기 위해 마트에 주어야 하는 병 수 a
+    // 빈 병 a개를 가져다 주면 마트가 주는 콜라 병 수 b
+    // 상빈이가 가지고 있는 빈 병의 개수 n
+    public int solution(int a, int b, int n) {
+        int result = 0;
+        
+        while(n / a != 0) {
+            result += (n / a) * b;
+            
+            n = (n / a) * b + n % a;
+        }
+        
+        return result;
+    }
+}
