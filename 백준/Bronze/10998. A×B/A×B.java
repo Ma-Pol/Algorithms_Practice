815f28b1639d963399f24da1a85e4a3fd76e33b7
Yi, Seung Hyeon <121948211+Ma-Pol@users.noreply.github.com>
2023-10-24 22:20:57 +0900

    [Bronze V] Title: A×B, Time: 124 ms, Memory: 14260 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        String nums = br.readLine();
+        
+        String[] numArr = nums.split(" ");
+        
+        System.out.print(Integer.parseInt(numArr[0]) * Integer.parseInt(numArr[1]));
+        
+        br.close();
+    }
+}
