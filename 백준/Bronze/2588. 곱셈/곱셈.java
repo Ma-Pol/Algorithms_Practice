9bff424359c5355ad3b676c0ac2d3ec9d87d909f
Yi, Seung Hyeon <121948211+Ma-Pol@users.noreply.github.com>
2023-10-24 22:53:33 +0900

    [Bronze III] Title: 곱셈, Time: 124 ms, Memory: 14140 KB -BaekjoonHub

--- 백준/Bronze/2588. 곱셈/곱셈.java
+++ 백준/Bronze/2588. 곱셈/곱셈.java
@@ -4,27 +4,13 @@ public class Main {
 	public static void main(String args[]) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int num1 = Integer.parseInt(br.readLine());
-        String[] num2 = (br.readLine()).split("");
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
         
-        int num3 = 0, num4 = 0, num5 = 0;
-        
-        for(String num : num2) {
-            int res = num1 * Integer.parseInt(num);
-            
-            if(num5 == 0) {
-                num5 = res;
-            } else if(num4 == 0) {
-                num4 = res;
-            } else {
-                num3 = res;
-            }
-        }
-        
-        System.out.println(num3);
-        System.out.println(num4);
-        System.out.println(num5);
-        System.out.println(num3 + num4 * 10 + num5 * 100);
+        System.out.println(a * (b % 10));
+        System.out.println(a * ((b % 100 - b % 10) / 10));
+        System.out.println(a * (b / 100));
+        System.out.println(a * b);
 
 		br.close();
 	}
