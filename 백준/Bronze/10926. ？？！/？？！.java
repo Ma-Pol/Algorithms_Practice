556f594a9e44150cf11ef5a15b945230b68e04f5
Yi, Seung Hyeon <121948211+Ma-Pol@users.noreply.github.com>
2023-10-24 22:30:03 +0900

    [Bronze V] Title: ??!, Time: 124 ms, Memory: 14280 KB -BaekjoonHub

--- 백준/Bronze/10926. ？？！/？？！.java
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -4,7 +4,8 @@ public class Main {
 	public static void main(String args[]) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        StringBuilder sb = new StringBuilder(br.readLine() + "??!");
+        StringBuilder sb = new StringBuilder(br.readLine());
+        sb.append("??!");
 
         System.out.print(sb.toString());
 
