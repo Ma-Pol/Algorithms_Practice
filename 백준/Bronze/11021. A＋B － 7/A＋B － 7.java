d3892bc410ca824eaea37b1afe641e802708ca95
Yi, Seung Hyeon <121948211+Ma-Pol@users.noreply.github.com>
2023-10-26 02:43:07 +0900

    [Bronze V] Title: A+B - 7, Time: 136 ms, Memory: 14292 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        
+        int T = Integer.parseInt(br.readLine());
+        
+        for(int i = 0; i < T; i++) {
+            String[] nums = (br.readLine()).split(" ");
+            int a = Integer.parseInt(nums[0]);
+            int b = Integer.parseInt(nums[1]);
+            
+            System.out.println(sb.append("Case #").append(i + 1).append(": ").append(a + b).toString());
+            sb.delete(0, sb.length());
+        }
+        
+        br.close();
+    }
+}
