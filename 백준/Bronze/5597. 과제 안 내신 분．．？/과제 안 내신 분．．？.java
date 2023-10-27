c0fb6e46cd5a79f9ca871b80cee722b13c5a901a
Yi, Seung Hyeon <121948211+Ma-Pol@users.noreply.github.com>
2023-10-27 11:48:41 +0900

    [Bronze V] Title: 과제 안 내신 분..?, Time: 128 ms, Memory: 14060 KB -BaekjoonHub

---
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
@@ -0,0 +1,31 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] students = new int[30];
+        int first = 0, second = 0;
+        
+        String student = br.readLine();
+        
+        while(student != null) {
+            int studentNum = Integer.parseInt(student);
+            
+            students[studentNum - 1] = 1;
+            
+            student = br.readLine();
+        }
+        
+        for(int i = 0; i < students.length; i++)
+            if(students[i] == 0)
+                if(first == 0)
+                    first = i + 1;
+                else
+                    second = i + 1;
+        
+        System.out.println(Math.min(first, second));
+        System.out.print(Math.max(first, second));
+        
+        br.close();
+    }
+}
