64544f074c4f881df96aa7c5162b76608b3a2a11
Yi, Seung Hyeon <121948211+Ma-Pol@users.noreply.github.com>
2023-11-04 01:33:59 +0900

    [level 1] Title: 기사단원의 무기, Time: 0.37 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
@@ -1,16 +1,12 @@
 class Solution {
     public int solution(int number, int limit, int power) {
         int result = 0;
-        int[] divisors = new int[number];
         
         for(int i = 0; i < number; i++) {
             int divisor = getDivisorNum(i + 1);
                 
-            divisors[i] = divisor > limit ? power : divisor;
+            result += divisor > limit ? power : divisor;
         }
-        
-        for(int weight : divisors)
-            result += weight;
         
         return result;
     }
