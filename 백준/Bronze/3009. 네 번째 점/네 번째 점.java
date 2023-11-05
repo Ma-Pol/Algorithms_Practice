6a147fc82e0b37911fcf67306b90708cd53eb857
Yi, Seung Hyeon <121948211+Ma-Pol@users.noreply.github.com>
2023-11-06 01:18:23 +0900

    [Bronze III] Title: 네 번째 점, Time: 124 ms, Memory: 14208 KB -BaekjoonHub

---
+++ 백준/Bronze/3009. 네 번째 점/네 번째 점.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        
+        int[][] coors = new int[2][3];
+        int[] result = new int[2];
+        
+        for(int i = 0; i < 3; i++) {
+            String[] input = (br.readLine()).split(" ");
+            coors[0][i] = Integer.parseInt(input[0]);
+            coors[1][i] = Integer.parseInt(input[1]);
+        }
+        
+        for(int i = 0; i < coors.length; i++) {
+            int[] coor = coors[i];
+            Arrays.sort(coor);
+            
+            sb.append(coor[0] == coor[1] ? coor[2] : coor[0]).append(" ");
+        }
+        
+        System.out.print(sb.toString());
+        
+        br.close();
+    }
+}
