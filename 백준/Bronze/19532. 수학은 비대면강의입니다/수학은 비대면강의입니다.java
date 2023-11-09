c3286c6d4db13a4b4f67483d8cd79f27daead643
Yi, Seung Hyeon <121948211+Ma-Pol@users.noreply.github.com>
2023-11-10 01:08:32 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 124 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.java
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.java
@@ -1,10 +1,10 @@
 import java.io.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        
+
         String[] input = (br.readLine()).split(" ");
         int a = Integer.parseInt(input[0]);
         int b = Integer.parseInt(input[1]);
@@ -12,17 +12,12 @@ public class Main {
         int d = Integer.parseInt(input[3]);
         int e = Integer.parseInt(input[4]);
         int f = Integer.parseInt(input[5]);
-        
-        loop:
-        for(int x = -999; x <= 999; x++)
-            for(int y = -999; y <= 999; y++)
-                if(a * x + b * y == c && d * x + e * y == f) {
-                    sb.append(x).append(" ").append(y);
-                    break loop;
-                }
-        
-        System.out.print(sb.toString());
 
-		br.close();
-	}
+        int y = (c * d - f * a) / (b * d - e * a);
+        int x = a == 0 ? ((f - e * y) / d) : ((c - b * y) / a);
+        
+        System.out.print(sb.append(x).append(" ").append(y));
+        
+        br.close();
+    }
 }
