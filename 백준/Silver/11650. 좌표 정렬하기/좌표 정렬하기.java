eed287365873bba29162f8f5a4cf98c799bb831e
Yi, Seung Hyeon <121948211+Ma-Pol@users.noreply.github.com>
2023-11-17 00:30:09 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 780 ms, Memory: 56040 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        
+        int N = Integer.parseInt(br.readLine());
+        String[] coorStringArr = new String[N];
+        int[][] coorArr = new int[N][2];
+        
+        // 좌표값 저장
+        for(int i = 0; i < N; i++) {
+            String[] input = (br.readLine()).split(" ");
+            
+            coorArr[i][0] = Integer.parseInt(input[0]);
+            coorArr[i][1] = Integer.parseInt(input[1]);
+        }
+        
+        // 정렬
+        Arrays.sort(coorArr, (coor1, coor2) -> {
+            // x좌표 값이 일치하면 y좌표값 기준으로 오름차순 정렬
+            if(coor1[0] == coor2[0])
+                return coor1[1] - coor2[1];
+            // x좌표 값이 다르면 x좌표값 기준으로 오름차순 정렬
+            else
+                return coor1[0] - coor2[0];
+        });
+        
+        for(int[] coor : coorArr)
+            sb.append(coor[0]).append(" ").append(coor[1]).append("\n");
+        
+        System.out.print(sb.toString());
+        
+        br.close();
+    }
+}
