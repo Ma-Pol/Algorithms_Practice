b4e2c11f269ad7bfe3685a35af78b83d30850a4f
Yi, Seung Hyeon <121948211+Ma-Pol@users.noreply.github.com>
2023-12-22 09:45:14 +0900

    [Bronze V] Title: 三方比較 (Three-Way Comparison), Time: 120 ms, Memory: 14152 KB -BaekjoonHub

---
+++ 백준/Bronze/27328. 三方比較 （Three－Way Comparison）/三方比較 （Three－Way Comparison）.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int A = Integer.parseInt(br.readLine());
+        int B = Integer.parseInt(br.readLine());
+        
+        System.out.print(A == B ? 0 : A > B ? 1 : -1);
+        
+        br.close();
+    }
+}
