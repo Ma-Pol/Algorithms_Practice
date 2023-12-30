75159083b1e00344f33a64d6a5af0c1fd816fe2e
Yi, Seung Hyeon <121948211+Ma-Pol@users.noreply.github.com>
2023-12-30 10:05:24 +0900

    [Bronze V] Title: Serca, Time: 336 ms, Memory: 62772 KB -BaekjoonHub

---
+++ 백준/Bronze/26766. Serca/Serca.java
@@ -0,0 +1,26 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        
+        int N = Integer.parseInt(br.readLine());
+        
+        while(N-- > 0) {
+              sb.append(" @@@   @@@ \n")
+                .append("@   @ @   @\n")
+                .append("@    @    @\n")
+                .append("@         @\n")
+                .append(" @       @ \n")
+                .append("  @     @  \n")
+                .append("   @   @   \n")
+                .append("    @ @    \n")
+                .append("     @     \n");
+        }
+        
+        System.out.print(sb.toString());
+        
+        br.close();
+    }
+}
