7f5f9eb9323ed814fe20ce21eedb077f7b4145b8
Yi, Seung Hyeon <121948211+Ma-Pol@users.noreply.github.com>
2023-12-31 10:19:28 +0900

    [Bronze V] Title: Julka, Time: 120 ms, Memory: 14296 KB -BaekjoonHub

--- 백준/Bronze/8437. Julka/Julka.java
+++ 백준/Bronze/8437. Julka/Julka.java
@@ -7,18 +7,21 @@ public class Main {
         
         BigInteger apples = new BigInteger(br.readLine());
         BigInteger diff = new BigInteger(br.readLine());
+        BigInteger zero = new BigInteger("0");
+        BigInteger one = new BigInteger("1");
+        BigInteger two = new BigInteger("2");
         BigInteger median = null;
         BigInteger Klaudia = null;
         BigInteger Natalia = null;
         
-        if(apples.remainder(new BigInteger("2")).equals(new BigInteger("0"))) {
-            median = apples.divide(new BigInteger("2"));
-            Klaudia = median.add(diff.divide(new BigInteger("2")));
-            Natalia = median.subtract(diff.divide(new BigInteger("2")));
+        if(apples.remainder(two).equals(zero)) {
+            median = apples.divide(two);
+            Klaudia = median.add(diff.divide(two));
+            Natalia = median.subtract(diff.divide(two));
         } else {
-            median = apples.divide(new BigInteger("2"));
-            Klaudia = median.add(diff.divide(new BigInteger("2"))).add(new BigInteger("1"));
-            Natalia = median.subtract(diff.divide(new BigInteger("2")));
+            median = apples.divide(two);
+            Klaudia = median.add(diff.divide(two)).add(one);
+            Natalia = median.subtract(diff.divide(two));
         }
         
         System.out.println(Klaudia);
