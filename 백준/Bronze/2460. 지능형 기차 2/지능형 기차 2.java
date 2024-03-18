f88c6cc69962ebf9aacc8d996d423264a0984c7a
Yi, Seung Hyeon <121948211+Ma-Pol@users.noreply.github.com>
2024-03-19 03:15:41 +0900

    [Bronze III] Title: 지능형 기차 2, Time: 120 ms, Memory: 14204 KB -BaekjoonHub

---
+++ 백준/Bronze/2460. 지능형 기차 2/지능형 기차 2.java
@@ -0,0 +1,24 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int maxPeople = 0;
+        int tempPeople = 0;
+        
+        for(int i = 0; i < 10; i++) {
+            String[] input = br.readLine().split(" ");
+            int getOff = Integer.parseInt(input[0]);
+            int getOn = Integer.parseInt(input[1]);
+            
+            tempPeople = tempPeople - getOff + getOn;
+            
+            if(maxPeople < tempPeople) maxPeople = tempPeople;
+        }
+        
+        System.out.print(maxPeople);
+        
+        br.close();
+    }
+}
