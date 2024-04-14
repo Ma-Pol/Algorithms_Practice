ca08462baf27ffb6468cfd11825c104e983a8b0d
Yi, Seung Hyeon <121948211+Ma-Pol@users.noreply.github.com>
2024-04-15 03:24:50 +0900

    [Bronze III] Title: 배수 찾기, Time: 124 ms, Memory: 14312 KB -BaekjoonHub

--- 백준/Bronze/4504. 배수 찾기/배수 찾기.java
+++ 백준/Bronze/4504. 배수 찾기/배수 찾기.java
@@ -12,10 +12,14 @@ public class Main {
             
             if(x == 0) break;
             
+            sb.append(x);
+            
             if(x % n == 0 && x >= n)
-                sb.append(x).append(" is a multiple of ").append(n).append(".\n");
+                sb.append(" is a multiple of ");
             else
-                sb.append(x).append(" is NOT a multiple of ").append(n).append(".\n");
+                sb.append(" is NOT a multiple of ");
+            
+            sb.append(n).append(".\n");
         }
         
         System.out.print(sb.toString());
