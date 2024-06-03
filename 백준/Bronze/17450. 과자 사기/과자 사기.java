a8b417a8913b6881371a5ba877daeb8ab0aa3256
Yi, Seung Hyeon <121948211+Ma-Pol@users.noreply.github.com>
2024-06-03 09:21:04 +0900

    [Bronze III] Title: 과자 사기, Time: 132 ms, Memory: 14180 KB -BaekjoonHub

--- 백준/Bronze/17450. 과자 사기/과자 사기.java
+++ 백준/Bronze/17450. 과자 사기/과자 사기.java
@@ -1,4 +1,4 @@
-/*import java.io.*;
+import java.io.*;
  
 public class Main {
     public static void main(String args[]) {
@@ -13,7 +13,7 @@ public class Main {
                 
                 double GSB = 0.0;
                 
-                GSB = price + 10 >= 5000 ? 
+                GSB = price * 10 >= 5000 ? 
                         (weight * 10) / (price * 10 - 500) : (weight * 10) / (price * 10);
                 
                 if(bestGSB < GSB) {
@@ -27,40 +27,5 @@ public class Main {
         } catch(IOException e) {
             e.getStackTrace();
         }
-    }
-}*/
-import java.util.Scanner;
- 
-public class Main {
-    public static void main(String args[]) {
-        
-        Scanner sc = new Scanner(System.in);
-        
-        double bestBangforTheBuck = 0;
-        String snack = "";
-        
-        for (int i = 1; i <= 3; i++) {
-            double price = sc.nextDouble();
-            double weight = sc.nextDouble();
-            
-            double bangForTheBuck = 0;
-            
-            if (price * 10 >= 5000) {
-                bangForTheBuck = (weight * 10) / (price * 10 - 500);
-            }
-            else {
-                bangForTheBuck = (weight * 10) / (price * 10 );
-            }
-            
-            if (bestBangforTheBuck < bangForTheBuck) {
-                bestBangforTheBuck = bangForTheBuck;
-                if (i == 1) snack = "S";
-                else if (i == 2) snack = "N";
-                else if (i == 3) snack = "U";
-            }
-        }
-        
-        System.out.print(snack);
- 
     }
 }
