372d1631d45a9556958e37121e17b6ca332d9660
Yi, Seung Hyeon <121948211+Ma-Pol@users.noreply.github.com>
2024-06-16 09:08:43 +0900

    [Bronze III] Title: 골뱅이 찍기 - ㄴ, Time: 132 ms, Memory: 14708 KB -BaekjoonHub

---
+++ 백준/Bronze/23803. 골뱅이 찍기 － ㄴ/골뱅이 찍기 － ㄴ.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            StringBuilder sb = new StringBuilder();
+            
+            int n = Integer.parseInt(br.readLine());
+            
+            sb.append(
+                ("@".repeat(n) + "\n").repeat(n * 4)
+            ).append(
+                ("@".repeat(n * 5) + "\n").repeat(n)
+            );
+            
+            System.out.print(sb.toString());
+        } catch(IOException e) {
+            e.getStackTrace();
+        }
+    }
+}
