9bb9a00410d99c562821db83c72c82a144f8a2ce
Yi, Seung Hyeon <121948211+Ma-Pol@users.noreply.github.com>
2024-06-22 05:40:07 +0900

    [Bronze III] Title: 골뱅이 찍기 - 돌아간 ㅍ, Time: 176 ms, Memory: 17560 KB -BaekjoonHub

---
+++ 백준/Bronze/23812. 골뱅이 찍기 － 돌아간 ㅍ/골뱅이 찍기 － 돌아간 ㅍ.java
@@ -0,0 +1,27 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            StringBuilder sb = new StringBuilder();
+            
+            int n = Integer.parseInt(br.readLine());
+            
+            sb.append(
+                ("@".repeat(n) + " ".repeat(n * 3) + "@".repeat(n) + "\n").repeat(n)
+            ).append(
+                ("@".repeat(n * 5) + "\n").repeat(n)
+            ).append(
+                ("@".repeat(n) + " ".repeat(n * 3) + "@".repeat(n) + "\n").repeat(n)
+            ).append(
+                ("@".repeat(n * 5) + "\n").repeat(n)
+            ).append(
+                ("@".repeat(n) + " ".repeat(n * 3) + "@".repeat(n) + "\n").repeat(n)
+            );
+            
+            System.out.print(sb.toString());
+        } catch(IOException e) {
+            e.getStackTrace();
+        }
+    }
+}
