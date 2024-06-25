d36fcf10d6dfa1c17510dfd98d2d393056300e32
Yi, Seung Hyeon <121948211+Ma-Pol@users.noreply.github.com>
2024-06-25 21:18:26 +0900

    [Bronze III] Title: 찬반투표, Time: 132 ms, Memory: 14508 KB, Score: 40 point -BaekjoonHub

---
+++ 백준/Bronze/27736. 찬반투표/찬반투표.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            int n = Integer.parseInt(br.readLine());
+            
+            int[] vote = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            
+            int agre = 0, oppo = 0, with = 0;
+            
+            for(int temp : vote)
+                if(temp == 1)
+                    agre++;
+                else if(temp == -1)
+                    oppo++;
+                else
+                    with++;
+            
+            if(n == 1)
+                System.out.print(agre == 1 ? "APPROVED" : oppo == 1 ? "REJECTED" : "INVALID");
+            else
+                System.out.print(n / 2 <= with ? "INVALID" : agre > oppo ? "APPROVED" : "REJECTED");
+        } catch(IOException e) {
+            e.getStackTrace();
+        }
+    }
+}
